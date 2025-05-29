1f2aaeed72146d13f790bdaba82955c7af4f687f
hxianggang <hunter930217@gmail.com>
2025-05-30 02:12:27 +0800

    vote顯示bug修好

--- vote.php
+++ vote.php
@@ -2,26 +2,31 @@
 include('header.php');
 include('db.php');
 
-// 處理搜尋功能
+// 處理搜尋關鍵字
 $search_keyword = '';
-if (isset($_GET['search'])) {
+$search_sql = '';
+if (isset($_GET['search']) && $_GET['search'] !== '') {
     $search_keyword = mysqli_real_escape_string($conn, $_GET['search']);
-    $sql = "SELECT * FROM event WHERE e_title LIKE '%$search_keyword%' OR e_text LIKE '%$search_keyword%' ORDER BY e_time DESC";
-} else {
-    $sql = "SELECT * FROM event ORDER BY e_time DESC";
+    $search_sql = " AND (e_title LIKE '%$search_keyword%' OR e_text LIKE '%$search_keyword%')";
 }
 
-$result = mysqli_query($conn, $sql);
+// 投票區 (e_type=0)
+$sql_vote = "SELECT * FROM event WHERE e_type=0 $search_sql ORDER BY e_time DESC";
+$result_vote = mysqli_query($conn, $sql_vote);
+
+// 已結束 (e_type=1)
+$sql_end = "SELECT * FROM event WHERE e_type=1 $search_sql ORDER BY e_time DESC";
+$result_end = mysqli_query($conn, $sql_end);
 ?>
 
 <!DOCTYPE html>
 <html lang="zh-TW">
 
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1" />
     <title>愛校建言系統</title>
-    <link rel="stylesheet" href="new.css">
+    <link rel="stylesheet" href="new.css" />
 </head>
 
 <body>
@@ -29,11 +34,10 @@ $result = mysqli_query($conn, $sql);
     <!-- 搜尋框 -->
     <form id="search-form" method="GET" class="index_search-block">
         <div class="index_search-box">
-            <input type="text" class="index_search-bar" placeholder="請輸入關鍵字" name="search" value="<?php echo htmlspecialchars($search_keyword); ?>">
+            <input type="text" class="index_search-bar" placeholder="請輸入關鍵字" name="search" value="<?php echo htmlspecialchars($search_keyword); ?>" />
             <button type="submit" class="index_search-but">搜尋</button>
         </div>
     </form>
-
 
     <!-- 投票列表 -->
     <div class="index_main-news">
@@ -41,46 +45,81 @@ $result = mysqli_query($conn, $sql);
             <div class="index_main-news-title_func open" id="index_title_func_1" onclick="OpenFunc1()">投票區</div>
             <div class="index_main-news-title_func" id="index_title_func_2" onclick="OpenFunc2()">已結束</div>
         </div>
-        <?php if ($result->num_rows > 0) { ?>
-            <?php while ($row = $result->fetch_assoc()) {
 
-                $sqlEtype = "SELECT e_type FROM event WHERE e_id = '{$row['e_id']}' LIMIT 1";
-                $resultEtype = mysqli_query($conn, $sqlEtype);
-                $e_type = 1;
-                if ($rowEtype = mysqli_fetch_assoc($resultEtype)) {
-                    $e_type = $rowEtype['e_type'];
-                }
-
-                $sql4 = "SELECT * FROM audit WHERE e_id = '{$row['e_id']}'";
-                $result4 = mysqli_query($conn, $sql4);
-                $row4 = mysqli_fetch_assoc($result4);
-
-                $eventDate = new DateTime($row['e_time']);
-                $now = new DateTime();
-                $interval = $eventDate->diff($now);
-                $isOverThreeMonths = ($interval->m + $interval->y * 12) >= 3;
+        <!-- 投票區 -->
+        <div id="vote-area">
+            <?php if ($result_vote && $result_vote->num_rows > 0) {
+                while ($row = $result_vote->fetch_assoc()) {
+                    // 計算投票數
+                    $sql3 = "SELECT COUNT(*) AS vote_count FROM vote WHERE e_id = '{$row['e_id']}'";
+                    $result3 = mysqli_query($conn, $sql3);
+                    $row3 = mysqli_fetch_assoc($result3);
             ?>
-                <div class="index_main-news-mess" data-etype="<?php echo $row['e_type']; ?>">
-                    <div class="index-mess-left" onclick="window.location.href='eve_post.php?e_id=<?php echo $row['e_id']; ?>'">
-                        <div class="index_mess-date"><?php echo $row['e_time']; ?></div>
-                        <div class="index_mess-title"><?php echo $row['e_title']; ?></div>
+                    <div class="index_main-news-mess" data-etype="0">
+                        <div class="index-mess-left" onclick="window.location.href='eve_post.php?e_id=<?php echo $row['e_id']; ?>'">
+                            <div class="index_mess-date"><?php echo $row['e_time']; ?></div>
+                            <div class="index_mess-title"><?php echo htmlspecialchars($row['e_title']); ?></div>
+                        </div>
+                        <div class="index-mess-right">
+                            <div class="index_mess-date">投票數：<?php echo $row3['vote_count']; ?></div>
+                        </div>
                     </div>
-                    <div class="index-mess-right">
-                        <?php
-                        $sql3 = "SELECT COUNT(*) AS vote_count FROM vote WHERE e_id = '{$row['e_id']}'";
-                        $result3 = mysqli_query($conn, $sql3);
-                        $row3 = mysqli_fetch_assoc($result3);
-                        ?>
-                        <div class="index_mess-date">投票數：<?php echo $row3['vote_count']; ?></div>
+            <?php
+                }
+            } else {
+                echo '<p>沒有符合條件的投票區建言。</p>';
+            } ?>
+        </div>
+
+        <!-- 已結束 -->
+        <div id="end-area" style="display:none;">
+            <?php if ($result_end && $result_end->num_rows > 0) {
+                while ($row = $result_end->fetch_assoc()) {
+                    // 計算投票數
+                    $sql3 = "SELECT COUNT(*) AS vote_count FROM vote WHERE e_id = '{$row['e_id']}'";
+                    $result3 = mysqli_query($conn, $sql3);
+                    $row3 = mysqli_fetch_assoc($result3);
+            ?>
+                    <div class="index_main-news-mess" data-etype="1">
+                        <div class="index-mess-left" onclick="window.location.href='eve_post.php?e_id=<?php echo $row['e_id']; ?>'">
+                            <div class="index_mess-date"><?php echo $row['e_time']; ?></div>
+                            <div class="index_mess-title"><?php echo htmlspecialchars($row['e_title']); ?></div>
+                        </div>
+                        <div class="index-mess-right">
+                            <div class="index_mess-date">投票數：<?php echo $row3['vote_count']; ?></div>
+                        </div>
                     </div>
-                </div>
-        <?php }
-        } ?>
-    </div>
+            <?php
+                }
+            } else {
+                echo '<p>沒有符合條件的已結束建言。</p>';
+            } ?>
+        </div>
     </div>
 
+    <script>
+        function OpenFunc1() {
+            // 顯示投票區，隱藏已結束
+            document.getElementById('vote-area').style.display = 'block';
+            document.getElementById('end-area').style.display = 'none';
 
-    <!-- 搜尋表單 (隱藏) -->
-    <form id="search-form" method="GET" style="display:none;">
-        <input type="text" name="search" value="<?php echo htmlspecialchars($search_keyword); ?>">
-    </form>
+            document.getElementById('index_title_func_1').classList.add('open');
+            document.getElementById('index_title_func_2').classList.remove('open');
+        }
+
+        function OpenFunc2() {
+            // 顯示已結束，隱藏投票區
+            document.getElementById('vote-area').style.display = 'none';
+            document.getElementById('end-area').style.display = 'block';
+
+            document.getElementById('index_title_func_2').classList.add('open');
+            document.getElementById('index_title_func_1').classList.remove('open');
+        }
+
+        // 預設打開投票區
+        OpenFunc1();
+    </script>
+
+</body>
+
+</html>
