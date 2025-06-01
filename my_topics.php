b9018e31bc44eac9c553644548c504a4590a312e
Arthur <tingyu0909online@gmail.com>
2025-06-01 17:03:38 +0800

    修正議題判定bug

--- my_topics.php
+++ my_topics.php
@@ -76,98 +76,117 @@ $result = mysqli_query($conn, $sql);
     <div class="mytopics_container">
         <h3 class="mytopics_heading">我的議題</h3>
 
-        <?php if ($result->num_rows > 0) { ?>
-            <?php while ($row = $result->fetch_assoc()) {
-                $eventDate = new DateTime($row['e_time']);
-                $now = new DateTime();
-                $interval = $eventDate->diff($now);
-                $isOverThreeMonths = ($interval->m + $interval->y * 12) >= 3; ?>
-                <?php if ($_SESSION['acc'] == $row['accounts']) { ?> <!--只有自己提的才能看到-->
-                    <div class="index_main-news-mess">
-                        <div class="index-mess-left" onclick="window.location.href='eve_post.php?e_id=<?php echo $row['e_id']; ?>'">
-                            <div class="index_mess-date"><?php echo $row['e_time']; ?></div>
-                            <div class="index_mess-title"><?php echo $row['e_title']; ?></div>
-                        </div>
-                        <div class="index-mess-right">
-                            <?php
-                            $e_id = $row['e_id'];
-                            $sqla = "SELECT * FROM audit WHERE e_id = $e_id";
-                            $resulta = mysqli_query($conn, $sqla);
-                            $rowa = mysqli_fetch_assoc($resulta);
-                            if ($isOverThreeMonths) {
-                                if (isset($rowa['a_acc'])) {
-                                    switch ($rowa['situation']) {
-                                        case 1:
-                                            echo "已審核通過";
-                                            break;
-                                        case 2:
-                                            echo "已否決建言";
-                                            break;
-                                        case 3:
-                                            echo "投票未通過";
-                                            break;
-                                        case 4:
-                                            echo "計畫制定中";
-                                            break;
-                                        case 5:
-                                            echo "正在募資中";
-                                            break;
-                                        case 6:
-                                            echo "計畫進行中";
-                                            break;
-                                        case 7:
-                                            echo "申訴期間";
-                                            break;
-                                        case 8:
-                                            echo "建言已結案";
-                                            break;
-                                    }
-                                } else {
-                                    echo "已結束待審核";
-                                }
-                            } else {
-                                if (isset($rowa['a_acc'])) {
-                                    switch ($rowa['situation']) {
-                                        case 1:
-                                            echo "已審核通過";
-                                            break;
-                                        case 2:
-                                            echo "已否決建言";
-                                            break;
-                                        case 3:
-                                            echo "投票未通過";
-                                            break;
-                                        case 4:
-                                            echo "計畫制定中";
-                                            break;
-                                        case 5:
-                                            echo "正在募資中";
-                                            break;
-                                        case 6:
-                                            echo "計畫進行中";
-                                            break;
-                                        case 7:
-                                            echo "申訴期間";
-                                            break;
-                                        case 8:
-                                            echo "建言已結案";
-                                            break;
-                                    }
-                                } else {
-                                    $sql3 = "SELECT COUNT(*) AS vote_count FROM vote WHERE e_id = '{$row['e_id']}'";
-                                    $result3 = mysqli_query($conn, $sql3);
-                                    $row3 = mysqli_fetch_assoc($result3);
-                                    echo "總投票數：", $row3['vote_count'];
-                                }
+
+
+        <?php
+if ($result->num_rows > 0) {
+    $hasPosted = false; // 新增旗標
+
+    while ($row = $result->fetch_assoc()) {
+        $eventDate = new DateTime($row['e_time']);
+        $now = new DateTime();
+        $interval = $eventDate->diff($now);
+        $isOverThreeMonths = ($interval->m + $interval->y * 12) >= 3;
+
+        if ($_SESSION['acc'] == $row['accounts']) { 
+            $hasPosted = true; 
+?>
+            <div class="index_main-news-mess">
+                <div class="index-mess-left" onclick="window.location.href='eve_post.php?e_id=<?php echo $row['e_id']; ?>'">
+                    <div class="index_mess-date"><?php echo $row['e_time']; ?></div>
+                    <div class="index_mess-title"><?php echo $row['e_title']; ?></div>
+                </div>
+                <div class="index-mess-right">
+                    <?php
+                    $e_id = $row['e_id'];
+                    $sqla = "SELECT * FROM audit WHERE e_id = $e_id";
+                    $resulta = mysqli_query($conn, $sqla);
+                    $rowa = mysqli_fetch_assoc($resulta);
+
+                    if ($isOverThreeMonths) {
+                        if (isset($rowa['a_acc'])) {
+                            switch ($rowa['situation']) {
+                                case 1:
+                                    echo "已審核通過";
+                                    break;
+                                case 2:
+                                    echo "已否決建言";
+                                    break;
+                                case 3:
+                                    echo "投票未通過";
+                                    break;
+                                case 4:
+                                    echo "計畫制定中";
+                                    break;
+                                case 5:
+                                    echo "正在募資中";
+                                    break;
+                                case 6:
+                                    echo "計畫進行中";
+                                    break;
+                                case 7:
+                                    echo "申訴期間";
+                                    break;
+                                case 8:
+                                    echo "建言已結案";
+                                    break;
                             }
-                            ?>
-                        </div>
-                    </div>
-                <?php } ?>
-            <?php } ?>
-        <?php } else { ?>
-            <p>您尚未發表過議題</p>
-        <?php } ?>
+                        } else {
+                            echo "已結束待審核";
+                        }
+                    } else {
+                        if (isset($rowa['a_acc'])) {
+                            switch ($rowa['situation']) {
+                                case 1:
+                                    echo "已審核通過";
+                                    break;
+                                case 2:
+                                    echo "已否決建言";
+                                    break;
+                                case 3:
+                                    echo "投票未通過";
+                                    break;
+                                case 4:
+                                    echo "計畫制定中";
+                                    break;
+                                case 5:
+                                    echo "正在募資中";
+                                    break;
+                                case 6:
+                                    echo "計畫進行中";
+                                    break;
+                                case 7:
+                                    echo "申訴期間";
+                                    break;
+                                case 8:
+                                    echo "建言已結案";
+                                    break;
+                            }
+                        } else {
+                            $sql3 = "SELECT COUNT(*) AS vote_count FROM vote WHERE e_id = '{$row['e_id']}'";
+                            $result3 = mysqli_query($conn, $sql3);
+                            $row3 = mysqli_fetch_assoc($result3);
+                            echo "總投票數：", $row3['vote_count'];
+                        }
+                    }
+                    ?>
+                </div>
+            </div>
+<?php
+        } 
+    } 
+
+    // 若沒有符合的資料，就顯示提示
+    if (!$hasPosted) {
+        echo "<p>您尚未發表過議題</p>";
+    }
+
+} else {
+    echo "<p>您尚未發表過議題</p>";
+}
+?>
+
+        
     </div>
 
     <!-- 只有學生身分才顯示新增建言按鈕 -->
