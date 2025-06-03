4396be8863a3f9d29fa684ef36346c56f0bf6fe5
Arthur <tingyu0909online@gmail.com>
2025-06-03 13:22:48 +0800

    session start

--- pro_process.php
+++ pro_process.php
@@ -1,5 +1,5 @@
 <?php
-if (session_status() == PHP_SESSION_NONE) //session_start();
+if (session_status() == PHP_SESSION_NONE) session_start();
 
 if (!isset($_SESSION['name'])) {
     header("Location: login.php");
